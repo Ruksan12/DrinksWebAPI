0eb7637d9c8a663492c922de6e65648f1b93bb41
Ruksan AC <ruksanarrashid@gmail.com>
2026-02-19 15:38:08 +0530

    fetch and displayed drink categories from CocktailDB API

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.Net.Http;
+using System.Runtime.InteropServices;
+using System.Security.Claims;
+using System.Security.Cryptography.X509Certificates;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+class Item
+{
+    public string strCategory { get; set; }
+}
+
+class Response
+{
+    public List<Item> drinks { get; set; }
+}
+class program
+{
+    static async Task Main()
+    {
+        HttpClient client = new HttpClient();
+        const string BaseURL = "https://www.thecocktaildb.com/api/json/v1/1";
+
+        var json = await client.GetStringAsync($"{BaseURL}/list.php?c=list");
+
+
+        var Result = JsonSerializer.Deserialize<Response>(json);
+
+        foreach (var item in Result.drinks)
+        {
+            Console.WriteLine(item.strCategory);
+        }
+    }
+}
+
+
+
+
